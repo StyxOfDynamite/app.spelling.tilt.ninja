a8f67cf8707f47aabb4cc99d49f20f62b3c7a8e8
lukes <lads89@gmail.com>
2021-02-22 21:42:45 +0000

    feat(icon): added ninja icon.

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,6 +1,27 @@
 import Box from './Box';
 
+const shuffle = (array) => {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
 const Boxes = (props) => {
+    const words = shuffle(props.words);
     return (
         <ol>
             {props.words.map((word, index) => (
