607e4e498d0e507bce65297ccb822df411f54795
lukes <lads89@gmail.com>
2021-02-24 20:27:04 +0000

    format(syllables) : tidy up

--- src/components/Syllables.js
+++ src/components/Syllables.js
@@ -18,14 +18,23 @@ const Syllables = (props) => {
         <div>
             {syls.map((syl, index) => (
                 <div key={`syllables-div-${index}`}>
-                    <h2 key={`syllables-header-${index}`}>{`${syl} Syllables`}</h2>
+                    <h2 key={`syllables-header-${index}`}>
+                        {`${syl} Syllables`}
+                    </h2>
                     <ol key={`syllables-list-${index}`}>
-                    {props.words.map((word, wordIndex) => {
-                        if (word.syllables === syl) {
-                            return <Underline key={`syllables-${wordIndex}`} mode="syllables" index={wordIndex} word={word.word} />;
-                        }
-                        return null;
-                    })}
+                        {props.words.map((word, wordIndex) => {
+                            if (word.syllables === syl) {
+                                return (
+                                    <Underline
+                                        key={`syllables-${wordIndex}`}
+                                        mode="syllables"
+                                        index={wordIndex}
+                                        word={word.word}
+                                    />
+                                );
+                            }
+                            return null;
+                        })}
                     </ol>
                 </div>
             ))}
